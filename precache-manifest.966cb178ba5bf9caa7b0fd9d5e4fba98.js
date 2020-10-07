714a4400349da33f7bbb5cd794fd37625cc34bcf
Eyezoh <amumpaireisaac@gmail.com>
2020-10-07 09:42:10 +0300

    Updates

---
+++ precache-manifest.966cb178ba5bf9caa7b0fd9d5e4fba98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182764da347bb3cae3ac23936be72c62",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "7f09cb341cdb0eb6fdc0",
+    "url": "/todo/static/css/main.04e1aae7.chunk.css"
+  },
+  {
+    "revision": "6ed3a38d346f65694319",
+    "url": "/todo/static/js/2.203cfefb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.203cfefb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f09cb341cdb0eb6fdc0",
+    "url": "/todo/static/js/main.d87aaf54.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
